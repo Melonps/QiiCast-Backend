f68ea1f43c8b10ed4b6f0a54da4c42d542d4163f
Melonps <kake.kake0311@gmail.com>
2024-01-16 08:50:34 +0000

    [FEAT] フロントエンドに情報を返すエンドポイントを追加

--- qiicast_backend/app.py
+++ qiicast_backend/app.py
@@ -1,20 +1,23 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi import FastAPI
-from fastapi import HTTPException
-import firebase_admin
 from pydantic import BaseModel  # リクエストbodyを定義するために必要
-from firebase_admin import credentials
-from firebase_admin import firestore
 from pathlib import Path
 from typing import Tuple
+from lib.inference.inference import InferenceEngine
+from lib.interact_qiita.qiita_api import request_page
 import uvicorn
-import datetime
 
+# FastAPIアプリケーションの初期化
 app = FastAPI()
 
-origins = ["http://localhost:3000", "http://localhost", "http://localhost:5173"]
-
+# CORSミドルウェアの設定
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:8502/",
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,199 +25,44 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-JSON_PATH = "admin-sdk.json"
 
+# InferenceEngineの初期化
+infer = InferenceEngine()
+infer._initialize_processor("nobo_model")
+# infer._initialize_processor("ik_model")
 
-cred = credentials.Certificate(JSON_PATH)
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-
-class PostData(BaseModel):
-    userid: str
-    username: str
-    coin: str
-    price: int
-    comment: str
-    tag: list = []
-    alignment: str
-
-
+# ルートエンドポイントへのGETリクエストに対するハンドラ
 @app.get("/")
 async def get():
+    """
+    ダミーのハローワールドレスポンスを返すエンドポイント
+    """
     return "Hello World"
 
+# /get_article エンドポイントへのGETリクエストに対するハンドラ
+@app.get("/get_article")
+async def get_article(tag: str, num_articles: int = 1):
+    """
+    Qiitaの記事情報を取得し、推論エンジンを使用して各記事の評価を予測するエンドポイント
+    :param tag: 取得したい記事のタグ
+    :param num_articles: 取得する記事の数（デフォルトは1）
+    :return: 記事情報と推論結果のリストをJSON形式で返す
+    """
+    response_data = []
+    meta_data_articles = request_page(tag, num_articles)
 
-@app.get("/get_file/{filename:path}")
-async def get_file(filename: str):
-    """任意ファイルのダウンロード"""
-    current = Path()
-    file_path = current / "files" / filename
-
-    response = FileResponse(path=file_path, filename=f"download_{filename}")
-
-    return response
-
-
-@app.post("/post_trading_record/")
-async def post_activity(data: PostData):
-    try:
-        db.collection("ActivityList").add(
+    for each_article in meta_data_articles:
+        # 推論エンジンを使用して記事の評価を予測
+        inference_result = infer.predict("nobo_model", each_article)
+        
+        # レスポンスデータに記事情報と推論結果を追加
+        response_data.append(
             {
-                "userid": data.userid,
-                "username": data.username,
-                "coin": data.coin,
-                "price": data.price,
-                "comment": data.comment,
-                "tag": data.tag,
-                "alignment": data.alignment,
-                "datetime": datetime.datetime.now(),
-                "up": True,
+                "title": each_article["title"],
+                "url": each_article["url"],
+                "rating": inference_result,
+                "created_at": each_article["created_at"],
+                "user": each_article["user"],
             }
         )
-    except Exception as e:
-        print("Error:", e)
-        return "エラーが発生しました"
-    return "ok"
-
-
-@app.get("/get_activity_list/{userid}")
-async def get_activity_list(userid: str):
-    try:
-        activity_list = []
-        if userid == "all":
-            docs = (
-                db.collection("ActivityList")
-                .order_by("datetime", direction=firestore.Query.DESCENDING)
-                .limit(30)
-                .get()
-            )  # 最大30件を取得
-            for doc in docs:
-                activity_list.append(doc.to_dict())
-            return activity_list
-        else:
-            docs = (
-                db.collection("ActivityList")
-                .where("userid", "==", userid)
-                .limit(30)
-                .get()
-            )  # 最大30件を取得
-            for doc in docs:
-                activity_list.append(doc.to_dict())
-            return activity_list
-
-    except Exception as e:
-        print("Error in get_activity_list:", e)
-        return "エラーが発生しました"
-
-
-@app.get("/get_summary/{range_start}/{range_end}")
-async def get_summary(range_start: int, range_end: int):
-    try:
-        docs = db.collection("Users").get()
-        users_coins_list = []
-        for doc in docs:
-            doc_data = doc.to_dict()
-            if "tag" in doc_data:
-                if (
-                    doc_data["state"] >= range_start * 10000
-                    and doc_data["state"] <= range_end * 10000
-                ):
-                    users_coins_list.extend(doc_data["tag"])
-        crypto_counts = {}
-        for crypto in users_coins_list:
-            if crypto in crypto_counts:
-                crypto_counts[crypto] += 1
-            else:
-                crypto_counts[crypto] = 1
-        response_data = []
-        for key, value in crypto_counts.items():
-            response_data.extend([{"name": key, "value": value}])
-        return response_data
-    except Exception as e:
-        print("Error in get_summary:", e)
-        return "エラーが発生しました"
-
-
-@app.post("/register_user/")
-async def register_user(data: dict):
-    try:
-        path = f"Users/{data['username']}"
-        doc = db.document(path).get()
-        if doc.exists:
-            print("すでに登録されています")
-            return "すでに登録されています"
-        else:
-            db.collection("Users").document(data["username"]).set(
-                {
-                    "username": data["username"],
-                    "state": data["state"],
-                    "target": data["target"],
-                    "tag": data["tag"],
-                }
-            )
-            return "ok"
-    except Exception as e:
-        print("Error:", e)
-        return "エラーが発生しました"
-
-
-@app.post("/create_user/")
-async def create_user(data: dict):
-    try:
-        path = f"Users/{data['userid']}"
-        doc = db.document(path).get()
-        if doc.exists:
-            raise HTTPException(status_code=409, detail="すでに登録されています")
-        else:
-            print(data["userid"])
-            doc_ref = db.collection("Users")
-            doc_ref.document(data["userid"]).set(
-                {
-                    "userid": data["userid"],
-                    "state": 0,
-                    "target": 0,
-                    "tag": ["Beginner"],
-                    "username": "Guest",
-                    "bio": "Hello, I'm a guest user!",
-                }
-            )
-            return {"message": "ユーザーが正常に作成されました"}
-    except Exception as e:
-        print("Error:", e)
-        raise HTTPException(status_code=500, detail="ユーザーの作成中にエラーが発生しました")
-
-
-@app.get("/get_user/{userid}")
-async def get_user(userid: str):
-    try:
-        doc = db.collection("Users").document(userid).get()
-        if doc.exists:
-            return doc.to_dict()
-        else:
-            raise HTTPException(status_code=404, detail="ユーザーが見つかりません")
-    except Exception as e:
-        print("Error:", e)
-        raise HTTPException(status_code=500, detail="ユーザーの取得中にエラーが発生しました")
-
-
-@app.post("/update_user/")
-async def update_user(data: dict):
-    try:
-        doc = db.collection("Users").document(data["userid"]).get()
-        if doc.exists:
-            db.collection("Users").document(data["userid"]).update(
-                {
-                    "username": data["username"],
-                    "state": data["state"],
-                    "target": data["target"],
-                    "tag": data["tag"],
-                    "bio": data["bio"],
-                }
-            )
-            return {"message": "ユーザーが正常に更新されました"}
-        else:
-            raise HTTPException(status_code=404, detail="ユーザーが見つかりません")
-    except Exception as e:
-        print("Error:", e)
-        raise HTTPException(status_code=500, detail="ユーザーの更新中にエラーが発生しました")
+    return response_data
