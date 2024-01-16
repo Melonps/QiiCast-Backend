f7e5792d29ad3fc47414295cb6ff9720e99453c4
Melonps <kake.kake0311@gmail.com>
2024-01-16 08:48:36 +0000

    [FIX] pathを読み取れるよう修正

--- qiicast_backend/lib/interact_qiita/qiita_api.py
+++ qiicast_backend/lib/interact_qiita/qiita_api.py
@@ -4,10 +4,11 @@ import requests
 from typing import Optional
 
 if True:
-    SCRT = yaml.safe_load(open("qiicast_backend/conf/secret.yml", "r"))
+    SCRT = yaml.safe_load(open("./conf/secret.yml", "r"))
     API_TOKEN = SCRT["qiita_v2"]["access_token"]
 
-def request_page(tag: Optional[str]) -> None:    
+
+def request_page(tag: Optional[str], num_articls: int) -> None:
     """Qiita APIから記事を取得し、JSON形式で保存する
 
     Parameters
@@ -15,13 +16,11 @@ def request_page(tag: Optional[str]) -> None:
     save_path : str
         保存先のパス
     tag : Optional[str]
-        タグ名。とりあえず高々1つ    
+        タグ名。とりあえず高々1つ
     """
 
-    headers = {
-        'Authorization': f'Bearer {API_TOKEN}'
-    }
-    url = f'https://qiita.com/api/v2/items?page=1&per_page=100'
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    url = f"https://qiita.com/api/v2/items?page=1&per_page={num_articls}"
     if tag:
         url += "&query=tag%3A" + tag
     response = requests.get(url, headers=headers)
